95778fc9babfaefbd1c6fe789855b27e9d8627ad
Исмаилов Алишер Алиаскарович <Alisher.Ismailov@norbit.ru>
2021-11-05 08:10:55 +0300

    Lesson 1 - fix CalculateTwoPointsDistance

--- GeekBrainsCSharpBasics/Helpers/FormulasHelper.cs
+++ GeekBrainsCSharpBasics/Helpers/FormulasHelper.cs
@@ -8,7 +8,7 @@ namespace GeekBrainsCSharpBasics
             weight / Math.Pow(height / 100, 2);
 
         public static double CalculateTwoPointsDistance(Point point1, Point point2) =>
-            Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point2.Y, 2));
+            Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point1.Y, 2));
 
         public static void Rearrangement(ref int value1, ref int value2)
         {
